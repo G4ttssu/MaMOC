521ab4ab5ac880893756e36f89ca8d4119660b53
Egor Zhelagin <zhelagin.egor@outlook.com>
2022-03-14 17:49:18 +0300

    finishing the Huffman algorithm

--- LR#4/src/MaMOC/Main.java
+++ LR#4/src/MaMOC/Main.java
@@ -1,36 +1,73 @@
 package MaMOC;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
+import java.lang.Comparable;
 
 public class Main {
 
-    static Map<Character,Integer> getDictionary(String input)
+    private static TreeMap<Character,Integer> createDictionary(String text)
     {
-        Map<Character,Integer> dictionary = new HashMap<Character,Integer>();
-        for(int index = 0; index < input.length(); ++index)
-            dictionary.putIfAbsent(input.charAt(index),getCharacters(input,input.charAt(index)));
+        TreeMap<Character,Integer> dictionary = new TreeMap<>();
+        for(int index = 0; index < text.length(); ++index)
+            dictionary.put(text.charAt(index), dictionary.get(text.charAt(index)) != null ? dictionary.get(text.charAt(index)) + 1 : 1 );
         return dictionary;
-    }
-
-    static int getCharacters(String input, Character symbol)
-    {
-        int count = 0;
-        for(int index = 0; index < input.length(); ++index)
-            if(symbol == input.charAt(index))
-                count++;
-        return count;
-    }
-
-    static void outDictionary(Map<Character,Integer> dictionary)
-    {
-        dictionary.entrySet().stream().sorted(Map.Entry.<Character,Integer>comparingByValue().reversed()).forEach(System.out::println);
     }
 
     public static void main(String[] args)
     {
-        String a = "abracadabra";
-        Map<Character,Integer> dictionary = getDictionary(a);
-        outDictionary(dictionary);
+        String text = "abracadabra 'dadasad' adasdadas' dasdasdadasdasdas eeqhfgf";
+        TreeMap<Character,Integer> dictionary = createDictionary(text);
+        ArrayList<Node> tree = new ArrayList<>();
+
+        for(Character character: dictionary.keySet())
+            tree.add(new Node(character, dictionary.get(character)));
+    }
+
+    private static Node huffmanCdde(ArrayList<Node> tree)
+    {
+        while(tree.size() > 1)
+        {
+            Collections.sort(tree);
+            Node left = tree.remove(tree.size() -1 );
+            Node right = tree.remove(tree.size() - 1);
+
+            Node parent = new Node(null, right.weight + left.weight, right, left);
+            tree.add(parent);
+        }
+
+        return tree.get(0);
+    }
+
+    private static class Node implements Comparable<Node>
+    {
+
+        Character symbol;
+        Integer weight;
+
+        Node right;
+        Node left;
+
+        public Node(Character symbol, Integer weight) {
+            this.symbol = symbol;
+            this.weight = weight;
+        }
+
+        public Node(Character symbol, Integer weight, Node right, Node left) {
+            this.symbol = symbol;
+            this.weight = weight;
+            this.right = right;
+            this.left = left;
+        }
+
+        public Node(Character symbol) {
+            this.symbol = symbol;
+        }
+
+        @Override
+        public int compareTo(Node object) {
+            return object.weight - weight;
+        }
     }
 }
